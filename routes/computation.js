a6f6c3468d3fe7a17240f7341f28032d47a56962
BHUVAN CHANDRA SARAKAM <s544899@nwmissouri.edu>
2021-10-27 19:19:45 -0500

    endpoint

--- routes/computation.js
+++ routes/computation.js
@@ -7,10 +7,7 @@ router.get('/', function(req, res, next) {
   a=Math.imul(edokati);
   b=Math.log(edokati);
   c=Math.log10(edokati);
-  res.send(`[Math.imul()] applied to ${edokati} is ${a}`);
-  res.send(`[Math.log()] applied to ${edokati} is ${b}`);
-  res.send(`[Math.log10()] applied to ${edokati} is ${c}`);
-});
+  res.send(`[Math.imul()] applied to ${edokati} is ${a} [Math.log()] applied to ${edokati} is ${b} [Math.log10()] applied to ${edokati} is ${c}`);
 
 module.exports = router;
 
