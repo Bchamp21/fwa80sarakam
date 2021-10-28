e2eccd9ffcb3849b44df08725bd8d21487e86d1b
BHUVAN CHANDRA SARAKAM <s544899@nwmissouri.edu>
2021-10-27 19:06:55 -0500

    edokati push

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var edokati = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(`User accesses are : ${edokati}`);
+  edokati++;
 });
 
 module.exports = router;
